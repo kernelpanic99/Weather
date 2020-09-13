8c1694c82ce393599f5648d1057e30c66a2c53fc
Artur Nikolaenko <nicolaenko99@gmail.com>
2020-09-13 21:29:33 +0300

    Updates

---
+++ precache-manifest.546301b11661e7dabbfa7da927e731d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7590c0065496422d7085b7a4c1b10c63",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "7920ecd92253e3faa69a",
+    "url": "/weather/static/css/main.479ebd1e.chunk.css"
+  },
+  {
+    "revision": "582b4887e2c2c23e8f23",
+    "url": "/weather/static/js/2.e61122b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather/static/js/2.e61122b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7920ecd92253e3faa69a",
+    "url": "/weather/static/js/main.2a05177b.chunk.js"
+  },
+  {
+    "revision": "65a135568c88d392bf25",
+    "url": "/weather/static/js/runtime-main.d8725fa2.js"
+  }
+]);
